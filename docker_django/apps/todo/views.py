2ba5868f35518352bc423344adf132c02164b63f
tobias-goertz <tobias.goertz96@gmail.com>
2021-12-07 18:13:52 +0100

    :recycle: restructure sample app

--- docker_django/apps/todo/views.py
+++ docker_django/apps/todo/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Item
-from redis import Redis
+from django.core.cache import cache
+import os
 
-
-redis = Redis(host='redis', port=6379)
 
 
 def home(request):
@@ -11,5 +10,6 @@ def home(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
     items = Item.objects.all()
-    counter = redis.incr('counter')
+    cache.get_or_set('counter', 0)
+    counter = cache.incr('counter')
     return render(request, 'home.html', {'items': items, 'counter': counter})
